95a53e4539930e0f94800bfab93625a7c09f0996
Julian Aron Prenner <furunkel@polyadic.com>
2021-12-02 17:35:59 +0100

    Some minor optimizations

--- ext/core/node.c
+++ ext/core/node.c
@@ -524,10 +524,17 @@ typedef struct {
 #define MAX(a,b) (((a)<(b))?(b):(a))
 #endif
 
+#define INDEX_LIST_ENTRY_CAPA 8
+typedef struct {
+  uint32_t len;
+  uint32_t next;
+  uint32_t data[INDEX_LIST_ENTRY_CAPA];
+} IndexListEntry;
+
 typedef struct {
   uint32_t capa;
   uint32_t len;
-  st_data_t *entries;
+  IndexListEntry *entries;
 } IndexList;
 
 // from st.c
@@ -575,30 +582,53 @@ static const struct st_hash_type type_table_entry_hash = {
     table_entry_hash,
 };
 
-// static IndexListEntry *
-// index_list_request_entry(IndexList *index_list) {
-//   if(!(index_list->len < index_list->capa)) {
-//     uint32_t new_capa = 2 * index_list->capa;
-//     RB_REALLOC_N(index_list->entries, IndexListEntry, new_capa);
-//     index_list->capa = new_capa;
-//   }
-//   IndexListEntry *entry = &index_list->entries[index_list->len];
-//   index_list->len++;
-//   return entry;
-// }
+static IndexListEntry *
+index_list_request_entry(IndexList *index_list, uint32_t *index) {
+  if(!(index_list->len < index_list->capa)) {
+    uint32_t new_capa = 2 * index_list->capa;
+    RB_REALLOC_N(index_list->entries, IndexListEntry, new_capa);
+    index_list->capa = new_capa;
+  }
+  IndexListEntry *entry = &index_list->entries[index_list->len];
+  *index = index_list->len;
+  index_list->len++;
+  return entry;
+}
 
+_Static_assert(sizeof(st_data_t) >= 2 * sizeof(uint32_t));
+
+#define MAKE_PAIR64(f, s) ((((st_data_t)(s)) << 32) | ((st_data_t)(f)))
+#define PAIR64_FIRST(p) ((p) & 0xFFFFFFFF)
+#define PAIR64_SECOND(p) ((p) >> 32)
 
 static uint32_t
 index_list_append(IndexList *index_list, st_data_t pair) {
-  if(!(index_list->len < index_list->capa)) {
-    uint32_t new_capa = 2 * index_list->capa;
-    RB_REALLOC_N(index_list->entries, st_data_t, new_capa);
-    index_list->capa = new_capa;
+  uint32_t value = PAIR64_FIRST(pair);
+  uint32_t index = PAIR64_SECOND(pair);
+
+  // new entry
+  if(index == UINT32_MAX) {
+    uint32_t new_index;
+    IndexListEntry *new_entry = index_list_request_entry(index_list, &new_index);
+    new_entry->next = UINT32_MAX;
+    new_entry->len = 1;
+    new_entry->data[0] = value;
+    return new_index;
+  } else {
+    IndexListEntry *entry = &index_list->entries[index];
+    if(!(entry->len < INDEX_LIST_ENTRY_CAPA)) {
+      uint32_t new_index;
+      IndexListEntry *new_entry = index_list_request_entry(index_list, &new_index);
+      new_entry->next = index;
+      new_entry->len = 1;
+      new_entry->data[0] = value;
+      return new_index;
+    } else {
+      entry->data[entry->len] = value;
+      entry->len++;
+      return index;
+    }
   }
-  uint32_t index = index_list->len;
-  index_list->entries[index] = pair;
-  index_list->len++;
-  return index;
 }
 
 //   if(entry == NULL) {
@@ -656,12 +686,6 @@ typedef struct {
   uint32_t value;
 } UpdateArg;
 
-_Static_assert(sizeof(st_data_t) >= 2 * sizeof(uint32_t));
-
-#define MAKE_PAIR64(f, s) ((((st_data_t)(s)) << 32) | ((st_data_t)(f)))
-#define PAIR64_FIRST(p) ((p) & 0xFFFFFFFF)
-#define PAIR64_SECOND(p) ((p) >> 32)
-
 int update_callback(st_data_t *key, st_data_t *value, st_data_t arg, int existing) {
   UpdateArg *update_arg = (UpdateArg *) arg;
   IndexList *index_list = update_arg->index_list;
@@ -677,6 +701,28 @@ int update_callback(st_data_t *key, st_data_t *value, st_data_t arg, int existin
   return ST_CONTINUE;
 }
 
+static inline void
+node_diff_step(uint32_t iold, uint32_t inew, st_table *_overlap, st_table *overlap,
+          uint32_t start_old, uint32_t start_new,
+          uint32_t *sub_length, uint32_t *sub_start_old, uint32_t *sub_start_new) {
+          st_data_t prev_sub_len = 0;
+
+          if(iold > start_old) {
+            st_lookup(overlap, (st_data_t) (iold - 1), (st_data_t *) &prev_sub_len);
+          }
+
+          uint32_t new_sub_len = prev_sub_len + 1;
+          if(new_sub_len > *sub_length) {
+              *sub_length = new_sub_len;
+              *sub_start_old = iold - (*sub_length) + 1;
+              *sub_start_new = inew - (*sub_length) + 1;
+          }
+          assert(*sub_length <= len_old);
+          assert(*sub_length <= len_new);
+          assert(*sub_start_old >= start_old);
+          assert(*sub_start_new >= start_new);
+          st_insert(_overlap, (st_data_t) iold, new_sub_len);
+}
 
 static void
 node_diff(VALUE rb_old, VALUE rb_new, IndexList *index_list, TableEntry *table_entries_old, st_table *overlap,
@@ -705,7 +751,7 @@ node_diff(VALUE rb_old, VALUE rb_new, IndexList *index_list, TableEntry *table_e
     st_update(old_index_map, (st_data_t) key, update_callback, (st_data_t) &arg);
   }
 
-  if(true || len_old > 0) {
+  if(len_old > 0) {
     for(size_t inew = start_new; inew < start_new + len_new; inew++) {
       VALUE rb_node = RARRAY_AREF(rb_new, inew);
       AstNode *node = (AstNode *) DATA_PTR(rb_node);
@@ -720,37 +766,31 @@ node_diff(VALUE rb_old, VALUE rb_new, IndexList *index_list, TableEntry *table_e
       st_data_t pair;
 
       if(st_lookup(old_index_map, (st_data_t)&key, &pair)) {
-        while(true) {
-          uint32_t value = PAIR64_FIRST(pair);
-          uint32_t next_index = PAIR64_SECOND(pair);
+        uint32_t value = PAIR64_FIRST(pair);
+        uint32_t next_index = PAIR64_SECOND(pair);
 
-          uint32_t iold = value;
+        node_diff_step(value, inew, _overlap, overlap, start_old, start_new,
+                  &sub_length, &sub_start_old, &sub_start_new);
 
-          st_data_t prev_sub_len = 0;
+        if(next_index != UINT32_MAX) {
+          IndexListEntry *entry = &index_list->entries[next_index];
+          IndexListEntry *iter = entry;
+          while(true) {
+            for(uint32_t i = 0; i < iter->len; i++) {
+              node_diff_step(iter->data[i], inew, _overlap, overlap, start_old, start_new,
+                        &sub_length, &sub_start_old, &sub_start_new);
+            }
 
-          if(iold > start_old) {
-            st_lookup(overlap, (st_data_t) (iold - 1), (st_data_t *) &prev_sub_len);
-          }
+            if(iter->next == UINT32_MAX) {
+              break;
+            }
 
-          uint32_t new_sub_len = prev_sub_len + 1;
-          if(new_sub_len > sub_length) {
-              sub_length = new_sub_len;
-              sub_start_old = iold - sub_length + 1;
-              sub_start_new = inew - sub_length + 1;
-          }
-          assert(sub_length <= len_old);
-          assert(sub_length <= len_new);
-          assert(sub_start_old >= start_old);
-          assert(sub_start_new >= start_new);
-          st_insert(_overlap, (st_data_t) iold, new_sub_len);
-
-          if(next_index == UINT32_MAX) {
-            break;
-          } else {
-            pair = index_list->entries[next_index];
+            // fprintf(stderr, "LOOP\n");
+            iter = &index_list->entries[iter->next];
           }
         }
       }
+
 
       {
         st_table *tmp = overlap;
@@ -773,6 +813,7 @@ node_diff(VALUE rb_old, VALUE rb_new, IndexList *index_list, TableEntry *table_e
     st_clear(_overlap);
     st_clear(overlap);
     st_clear(old_index_map);
+    index_list->len = 0;
 
     assert(sub_length <= len_old);
     assert(sub_length <= len_new);
@@ -792,6 +833,7 @@ node_diff(VALUE rb_old, VALUE rb_new, IndexList *index_list, TableEntry *table_e
     st_clear(_overlap);
     st_clear(overlap);
     st_clear(old_index_map);
+    index_list->len = 0;
 
     node_diff(rb_old, rb_new, index_list, table_entries_old, overlap, _overlap, old_index_map, input_old, input_new,
               sub_start_old + sub_length, (start_old + len_old) - (sub_start_old + sub_length),
@@ -824,13 +866,13 @@ rb_node_diff_s(VALUE self, VALUE rb_old, VALUE rb_new, VALUE rb_output_eq) {
   IndexList index_list;
   index_list.capa = len_old;
   index_list.len = 0;
-  index_list.entries = RB_ALLOC_N(st_data_t, index_list.capa);
+  index_list.entries = RB_ALLOC_N(IndexListEntry, index_list.capa);
 
   VALUE rb_out_ary = rb_ary_new_capa(MAX(len_old, len_new));
 
-  st_table *overlap = st_init_numtable_with_size(len_new);
-  st_table *_overlap = st_init_numtable_with_size(len_new);
-  st_table *old_index_map = st_init_table_with_size(&type_table_entry_hash, len_new / 4);
+  st_table *overlap = st_init_numtable();
+  st_table *_overlap = st_init_numtable();
+  st_table *old_index_map = st_init_table(&type_table_entry_hash);
 
   node_diff(rb_old, rb_new, &index_list, table_entries_old, overlap, _overlap, old_index_map,
             input_old, input_new, 0, len_old, 0, len_new, rb_out_ary, output_eq);
